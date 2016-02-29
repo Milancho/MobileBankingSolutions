0362c6cabe58393515923788fc1f7a9007b184b6
Milancho <milancho.arsovski@gmail.com>
2016-02-29 12:24:26 +0100

    tri opcii za prezemanje na podatoci od Web Api

--- MobileBankingSolutions/views/Register/MenuEagerView.js
+++ MobileBankingSolutions/views/Register/MenuEagerView.js
@@ -3,8 +3,10 @@
     var url = "http://localhost:8080/api/menu";
     var myData;
 
-var viewModel = {
-   
+    var viewModel = {
+        loadPanelVisible: ko.observable(false),
+
+    // (1)
     //dataSource: new DevExpress.data.DataSource({
     //    load: function (loadOptions) {
     //        var d = $.Deferred();
@@ -15,45 +17,46 @@ var viewModel = {
     //        return d.promise();
     //    }
     //}),
-   dataSource: ko.observable(new DevExpress.data.DataSource),
-    Search_Click: function () {
-    
-        // prikazija
-        DevExpress.ui.notify('Item Loaded', 'success', 3000);
-        var d1 = $.Deferred();
-        $.when(d1).done(function (v1) {
-            // trgnija
-            viewModel.dataSource(v1.Data);
+
+    // (2)
+    //dataSource: ko.observable(new DevExpress.data.DataSource),
+    Search_Click: function() {
+    //    this.loadPanelVisible(true);
+    //    var d1 = $.Deferred();
+    //    $.when(d1).done(function (v1) {
+    //        viewModel.loadPanelVisible(false);
+    //        viewModel.dataSource(v1.Data);
+    //    });
+
+    //    $.ajax({
+    //        type: 'GET',
+    //        url: url,
+    //        contentType: 'application/json; charset = utf - 8',
+    //        dataType: 'json',
+    //        success: function (data) {
+    //            d1.resolve(data, { totalCount: data.length });
+    //        }
+    //    });
+     },
+
+    //(3)
+    dataSource: new DevExpress.data.DataSource({
+        load: function (loadOptions) {
+            var d = $.Deferred();
+            console.log('url:' + url);
             
-        });
-
-        $.ajax({
-            type: 'GET',
-            url: url,
-            contentType: 'application/json; charset = utf - 8',
-            dataType: 'json',
-            success: function (data) {
-                d1.resolve(data, { totalCount: data.length });
-            }
-        });
-     }
-    //dataSource: new DevExpress.data.DataSource({
-    //    load: function (loadOptions) {
-    //        var d = $.Deferred();
-    //        console.log(url + '11');
-
-    //        $.ajax({
-    //            type: 'GET',
-    //            url: url,
-    //            contentType: 'application/json; charset = utf - 8',
-    //            dataType: 'json',
-    //            success: function (data) {
-    //                d.resolve(data.Data, { totalCount: data.Data.length });
-    //            }
-    //        });
-    //        return d.promise();
-    //    }
-    //})
+            $.ajax({
+                type: 'GET',
+                url: url,
+                contentType: 'application/json; charset = utf - 8',
+                dataType: 'json',
+                success: function (data) {
+                    d.resolve(data.Data, { totalCount: data.Data.length });
+                }
+            });
+            return d.promise();
+        }
+    })
 };
 return viewModel;
 };
