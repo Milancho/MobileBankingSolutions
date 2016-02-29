5e6036ca2e7851ff196dd0f9cbc0a035e2caaaef
Milancho <milancho.arsovski@gmail.com>
2016-02-29 15:10:16 +0100

    post login

--- MobileBankingSolutions/views/home.js
+++ MobileBankingSolutions/views/home.js
@@ -1,33 +1,82 @@
 ﻿MobileBankingSolutions.home = function (params) {
 
+    var url = "http://localhost:8080/token";
     var viewModel = {
         login: ko.observable(""),
         password: ko.observable(""),
         validateAndSubmit: function (params) {
-            var item = 'a';
-            if (this.login() == item) {
-                var result = params.validationGroup.validate();
-                if (result.isValid) {
-                    DevExpress.ui.notify({
-                        message: "Successful Login - '" + this.login() + "'",
-                        position: {
-                            my: "left top",
-                            at: "left top",
-                            offset: 15
-                        }
-                    }, "success", 150);
-                    MobileBankingSolutions.app.navigate("MenuView");
-                }
-            } else {
+            $.post(url, { grant_type: "password", username: this.login(), password: this.password() }).done(function () {
+                //alert("Data: " + data.access_token + "\nStatus: " + status);
                 DevExpress.ui.notify({
-                    message: "Invalid Username and/or Password",
+                    message: "Successful Login - '" + viewModel.login() + "'",
                     position: {
                         my: "left top",
                         at: "left top",
                         offset: 15
                     }
-                }, "warning", 1000);
-            }
+                }, "success", 300);
+                MobileBankingSolutions.app.navigate("MenuView");
+                }).fail(function () {
+                    DevExpress.ui.notify({
+                        message: "Invalid Username and/or Password",
+                        position: {
+                            my: "left top",
+                            at: "left top",
+                            offset: 15
+                        }
+                    }, "warning", 1000);
+                });
+            
+            //, function (data, status) {alert("Data: " + data.access_token + "\nStatus: " + status);
+            //$.post(url, { grant_type: "password", username: "aspekt1", password: "aspekt" }).done(function () {
+            //    alert("Thank you for registering!");
+            //}).fail(function () {
+            //    alert("Email already exists");
+            //});
+            
+            //if (this.login() == item) {
+            //    var result = params.validationGroup.validate();
+            //    if (result.isValid) {
+            //        DevExpress.ui.notify({
+            //            message: "Successful Login - '" + this.login() + "'",
+            //            position: {
+            //                my: "left top",
+            //                at: "left top",
+            //                offset: 15
+            //            }
+            //        }, "success", 150);
+            //        MobileBankingSolutions.app.navigate("MenuView");
+            //    }
+            //} else {
+            //    DevExpress.ui.notify({
+            //        message: "Invalid Username and/or Password",
+            //        position: {
+            //            my: "left top",
+            //            at: "left top",
+            //            offset: 15
+            //        }
+            //    }, "warning", 1000);
+            //}
+            //});
+            
+            //var jqxhr = $.post("example.php", function () {
+            //    alert("success");
+            //})
+
+            //$.ajax({
+            //    type: 'POST',
+            //    url: url,
+            //    data: {
+            //        'grant_type': 'password',
+            //        'username': 'aspekt',
+            //        'password': 'aspekt'
+            //    },
+            //    contentType: 'application/json; charset = utf - 8',
+            //    success: function (data) {
+            //        console.log(data);
+            //    }
+            //});
+            
         },
         message: ko.observable('Welcome !'),
         name: ko.observable(''),
